2ab49cece84515b83a914c53f98afd24e32d9f7c
EllenPinheiro <ellen.pinheiro2@estudante.ifms.edu.br>
2023-08-30 08:16:06 -0400

    salvando modificações

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,11 +1,20 @@
 import './Main.css'
 import EllenLinda from '..//img/ellen-linda (1).jpeg'
+import Ellen1 from '..//img/ellen-linda (2).jpeg'
+import Ellen2 from '..//img/WhatsApp Image 2023-08-30 at 08.00.55.jpeg'
+import Ellen3 from '..//img/ellen-linda (3).jpeg'
 import Portfolio from '..//img/portfolio-removebg-preview.png'
-
 
 function Main(){
     return(
-        <div className="main-content">
+        <div className='content-all'>
+        <div className='content-fotos'>
+            <img src={Ellen1} alt=""></img>
+            <img src={Ellen2} alt=""></img>
+            <img src={Ellen3} alt=""></img>
+        </div>
+
+<div className="main-content">
             <div className='portfolio'>
 <img src={Portfolio} alt=""></img>
 </div>
@@ -19,6 +28,7 @@ Sou uma garota cristã, otimista, dedicada, sonhadora, extrovertida e estou <br 
 <div className='fotoellenlinda'>
     <img  src={EllenLinda} alt="Eu"></img>
     </div>
+        </div>
         </div>
     )
 }
