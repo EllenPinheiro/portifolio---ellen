1c3f0ef4768d663248bfad208ab7c6770afed2cc
EllenPinheiro <ellen.pinheiro2@estudante.ifms.edu.br>
2023-09-19 08:49:45 -0400

    salvando arrumações

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -6,6 +6,7 @@ import Ellen2 from '..//img/WhatsApp Image 2023-08-30 at 08.00.55.jpeg'
 import Ellen3 from '..//img/ellen-linda (3).jpeg'
 import Portfolio from '..//img/portfolio-removebg-preview.png'
 import Projeto from './../projetos/Projeto'
+import Lupa from '..//img/download.png'
 
 type ProjetoType = {
     id:number,
@@ -73,9 +74,34 @@ export default function Main(){
           //console.log(e.target.value)
           //Como eu faço para mudar o texto para "TERE"
           setTexto(e.target.value)
-    return(
-<>
-<div className="campo_pesquisa">
+      }
+    return (
+        <>
+        <div className='content-all'>
+<div className='content-fotos'>
+    <img src={Ellen1} alt=""></img>
+    <img src={Ellen2} alt=""></img>
+    <img src={Ellen3} alt=""></img>
+</div>
+
+<div className="main-content">
+    <div className='portfolio'>
+<img src={Portfolio} alt=""></img>
+</div>
+ <div className='sobre-mim'>
+    <p className='titulo-sobremim'>QUEM SOU EU?</p>
+    <p className='text-sobremim'> Eu me chamo Ellen Vasconcelos Pinheiro,
+tenho 18 anos e <br /> atualmente estou terminando o 3º ano do ensino médio, <br />no curso de Técnico em Informática para Internet no IFMS. <br />
+Moro na cidade de Naviraí-MS, com os meus pais e minhas <br />duas irmãs mais novas. Namoro faz 3 anos com meu namorado <br /> lindo Jonas.
+Sou uma garota cristã, otimista, dedicada, sonhadora,<br /> extrovertida e estou em constante busca pela felicidade, louvar e <br />adorar meu Senhor Jesus. Eu sonho em ter o meu emprego dos <br /> sonhos, formar uma família e viajar o mundo!</p>
+</div>
+
+<div className='fotoellenlinda'>
+<img  src={EllenLinda} alt="Eu"></img>
+</div>
+</div>
+</div>
+            <div className="campo_pesquisa">
                 <p>Busque um filme:</p>
                 <div className='quadradinho'>
                 <input type="text" 
@@ -88,37 +114,11 @@ export default function Main(){
                 </div>
                 {texto && <p className='text-resultados'>Exibindo resultados para: {texto} </p>}
             </div>
-
-        <div className='content-all'>
-        <div className='content-fotos'>
-            <img src={Ellen1} alt=""></img>
-            <img src={Ellen2} alt=""></img>
-            <img src={Ellen3} alt=""></img>
-        </div>
-
-<div className="main-content">
-            <div className='portfolio'>
-<img src={Portfolio} alt=""></img>
-</div>
-         <div className='sobre-mim'>
-            <p className='titulo-sobremim'>QUEM SOU EU?</p>
-            <p className='text-sobremim'> Eu me chamo Ellen Vasconcelos Pinheiro,
-tenho 18 anos e <br /> atualmente estou terminando o 3º ano do ensino médio, <br />no curso de Técnico em Informática para Internet no IFMS. <br />
-Moro na cidade de Naviraí-MS, com os meus pais e minhas <br />duas irmãs mais novas. Namoro faz 3 anos com meu namorado <br /> lindo Jonas.
-Sou uma garota cristã, otimista, dedicada, sonhadora,<br /> extrovertida e estou em constante busca pela felicidade, louvar e <br />adorar meu Senhor Jesus. Eu sonho em ter o meu emprego dos <br /> sonhos, formar uma família e viajar o mundo!</p>
-</div>
-
-<div className='fotoellenlinda'>
-    <img  src={EllenLinda} alt="Eu"></img>
-    </div>
-        </div>
-
-        <>
             <main className="content-main">
 
             {projetos.filter((projeto) => {
     const textoSemEspacos = texto.replace(/\s/g, ''); // Remove espaços em branco do texto
-    const tituloSemEspacos = projeto.titulo.replace(/\s/g, ''); // Remove espaços em branco do título do filme
+    const tituloSemEspacos = projeto.titulo.replace(/\s/g, ''); // Remove espaços em branco do título do projeto
     return tituloSemEspacos.toLowerCase().includes(textoSemEspacos.toLowerCase());
                      })
                      .map((projeto) => (
@@ -126,9 +126,7 @@ Sou uma garota cristã, otimista, dedicada, sonhadora,<br /> extrovertida e esto
                      ))}
 
       </main>
-    
     </>
-        </div>
-    )
+  )
 }
-}
+
