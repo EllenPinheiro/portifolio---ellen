00203cffbc59cf3007835f23d202802831cefb67
EllenPinheiro <ellen.pinheiro2@estudante.ifms.edu.br>
2023-10-17 10:16:22 -0400

    salvando coisinhas

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -12,25 +12,27 @@ export default function Contato() {
             <div>
                 <img className='fotominha' src={EuLinda} alt="" />
             </div>
+            
         <form className='formulario' action="/contato">
+        <h1 className='titulo'>Olá! Entre em contato comigo por aqui :)</h1>
             <div>
             <label htmlFor="nome">Nome:</label>
-            <input type="text" />
+            <input type="text" placeholder='Digite seu nome!' required/>
             </div>
             <div>
             <label htmlFor="email">E-mail:</label>
-            <input type="email" />
+            <input type="email" placeholder='Informe seu e-mail' required/>
             </div>
-            <div>
+            <div className='final-dapartedecima'>
             <label htmlFor="telefone">Telefone:</label>
-            <input type="tel" />
+            <input type="tel" id="phone" name="phone" pattern="\(\d{2}\) \d{5}-\d{4}" required placeholder='Digite no formato: (xx) xxxxx-xxxx'/>
             </div>
-            <div>
             <label htmlFor="texto">Digite sua mensagem:</label>
-            <input type="text" />
+            <div>
+            <input className='mensagem' type="text" placeholder='Fale comigo!' required/>
             </div>
             <div className="button">
-            <button type="submit">Enviar</button>
+            <button className='button' type="submit">Enviar</button>
             </div>
         </form>
         </main>
